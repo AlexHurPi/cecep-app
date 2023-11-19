d4e1fbc58c7d928ef4b475491007e01db6dc99ab
Alexander Hurtado <alexander.hurtado7170@gmail.com>
2023-11-19 14:54:42 -0500

    index estudiantes y ajustes

--- database/migrations/2023_11_19_165904_create_estudiantes_table.php
+++ database/migrations/2023_11_19_165904_create_estudiantes_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('students', function (Blueprint $table) {
+        Schema::create('estudiantes', function (Blueprint $table) {
             $table->id();
             $table->string('nombre', 64);
             $table->string('apellido', 64);
