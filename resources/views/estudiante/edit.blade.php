29d6db9c32b5cb5f018a8e027dc1c0d619a9c0a4
Alexander Hurtado <alexander.hurtado7170@gmail.com>
2023-11-19 19:27:56 -0500

    edit estudiantes

--- resources/views/estudiante/edit.blade.php
+++ resources/views/estudiante/edit.blade.php
@@ -9,22 +9,22 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     
 
-    <title>Agregar Estudiante</title>
+    <title>Editar Estudiantes</title>
   </head>
   <body>
     <x-app-layout>
       <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-          {{ __('Agregar Estudiante') }} </h2>
+          {{ __('Editar Estudiante') }} </h2>
         </x-slot>
         <div class="py-12">
           <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
             <div class="p-6 text-gray-900">
             
-              <div class="container mb-3">    
-
-    <form method="POST" action="{{ route('estudiantes.store') }}">
-      @csrf
+              <div class="container mb-3">
+    <form method="POST" action="{{ route('estudiantes.update', ['estudiante'=>$estudiante->id]) }}">
+      @method('put')
+        @csrf
       <div class="mb-2">
         <label for="id" class="form label">Id</label>
         <input type="text" class="form-input rounded focus:outline-none w-full bg-gray-100" 
@@ -70,13 +70,17 @@
       <select class="form-select w-60 rounded-md focus:outline-none" 
       id="carreraid" name="carreraid" required>
         <option selected disabled value="">Elija una carrera...</option>
-        @foreach ($carreras as $carrera)       
+        @foreach ($carreras as $carrera)
+        @if ($carreras->id ==$estudiantes->carreraid)
         <option value="{{$carrera->id}}">{{$carrera->nombre}}</option>
-        
+        @else
+       <option value="{{$carrera->id}}">{{$carrera->nombre}}</option>
+          
+      @endif
       @endforeach
       </select>
         <div class="mt-3">
-        <button type="submit" class="btn btn-info" >Guardar</button>
+        <button type="submit" class="btn btn-info" > Actualizar</button>
           <a href="{{route('estudiantes.index')}}" class="btn btn-warning">Cancelar</a>        
       </div>      
     </form>
